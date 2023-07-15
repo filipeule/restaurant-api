e01590934219107ba5ee6093c7e4a735e532b3d9
filipeule <filipe_ule@hotmail.com>
2023-07-15 17:05:26 -0300

    Adicionado schema e model do Type no banco de dados. Alterado inicialização do banco para carregar os tipos dos produtos na inicialização do servidor

--- src/server.js
+++ src/server.js
@@ -7,8 +7,12 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-await mongo.connect();
+try {
+  await mongo.startDatabase();
 
-server.listen(PORT, () => {
-  console.log(`Listening on: http://localhost:${PORT}`);
-});
+  server.listen(PORT, () => {
+    console.log(`Listening on: http://localhost:${PORT}`);
+  });
+} catch (error) {
+  console.error(error.message);
+}
--- src/services/mongo.js
+++ src/services/mongo.js
@@ -1,6 +1,13 @@
 import mongoose from 'mongoose';
+import { populateTypesCollection } from '../models/types.model.js'
 
 const MONGO_URL = process.env.MONGO_URL;
+
+async function startDatabase() {
+  await connect();
+  
+  await populateTypesCollection();
+}
 
 async function connect() {
   await mongoose.connect(MONGO_URL);
@@ -23,6 +30,7 @@ mongoose.connection.on('error', (err) => {
 });
 
 export {
+  startDatabase,
   connect,
   disconnect,
 }
